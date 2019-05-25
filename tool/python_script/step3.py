568e436cdd5166115ab33cbed542cf2549d80f4d
Minxuan Sun <sunminxuan1997@gmail.com>
2019-05-24 21:44:23 -0500

    fix step3: delete if statement

--- tool/python_script/step3.py
+++ tool/python_script/step3.py
@@ -116,8 +116,6 @@ column = 0
 # convert DNA_matrix_data to the matrix
 while row < len(DNA_matrix_data):
     DNA_matrix_data[row] = DNA_matrix_data[row].split()
-    if DNA_matrix_data[row][-1] == '\n' or DNA_matrix_data[row][-1] == '\t':
-        DNA_matrix_data[row] = DNA_matrix_data[row][0:-1]
     row = row + 1
 
 # start calculating TPM
@@ -187,8 +185,6 @@ column = 0
 # convert DNA_matrix_data to the matrix
 while row < len(RNA_matrix_data):
     RNA_matrix_data[row] = RNA_matrix_data[row].split()
-    if RNA_matrix_data[row][-1] == '\n' or RNA_matrix_data[row][-1] == '\t':
-        RNA_matrix_data[row] = RNA_matrix_data[row][0:-1]
     row = row + 1
 
 # start calculating TPM
